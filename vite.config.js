16f6227e4952b3636ae3af975fecbfc077a1fee6
Elvis Mao <info@elvismao.com>
2024-12-26 16:12:43 +0800

    for the build.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html')
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about/index.html'),
+        "zh-Hant": resolve(__dirname, 'zh-Hant/index.html'),
+        "zh-Hant/about": resolve(__dirname, 'zh-Hant/about/index.html'),
       },
     },
   },
