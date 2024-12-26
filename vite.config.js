72d1bbc427172f5863e8abcbc35e8a44fdf1e9b6
Elvis Mao <info@elvismao.com>
2024-12-26 16:00:38 +0800

    fix rendering lang and en redirect

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
@@ -6,8 +5,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'about/index.html'),
+        main: resolve(__dirname, 'index.html')
       },
     },
   },
