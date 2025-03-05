a00b84978dc7a3f5969c82297a41dfe2d1db1eb0
Elvis Mao <info@elvismao.com>
2025-03-05 09:20:30 +0800

    fix redirect and seo

--- src/style.js
+++ src/style.js
@@ -22,57 +22,53 @@ if (!supportedLangs.includes(window.location.pathname.split("/")[1])) {
     lang = supportedLangs.includes(lang) ? lang : "en";
     // when page loaded
     window.location.href = `/${lang}${window.location.pathname}`;
-} else {
-    document.getElementById("toggleLang").addEventListener("click", e => {
-        const newLang = lang === "en" ? "zh-Hant" : "en";
-        setCookie("lang", newLang, 365);
-    });
-
-    // if any <a> been pressed, set body opacity = 0
-    document.querySelectorAll("a").forEach(a => {
-        a.addEventListener("click", e => {
-            e.preventDefault();
-            let a = e.target.closest("a"); // Find the closest <a> element (in case of nested elements)
-            if (!a) return;
-            console.log(a);
-            if (
-                a.getAttribute("target") !== "_blank" &&
-                !e.ctrlKey &&
-                !e.metaKey
-            ) {
-                document.body.style.opacity = 0;
-                setTimeout(() => {
-                    window.location.href = a.href;
-                }, 300);
-            } else {
-                window.open(a.href, "_blank");
-            }
-        });
-    });
-
-    // aos
-
-    function isElementInViewport(el) {
-        const rect = el.getBoundingClientRect();
-        return rect.bottom < 0 || rect.top > window.innerHeight;
-    }
-
-    function addClassToVisibleElements() {
-        var aosElements = document.querySelectorAll(".aos, li");
-        aosElements.forEach(function (aosElement) {
-            if (!isElementInViewport(aosElement))
-                aosElement.classList.add("ed");
-            else aosElement.classList.remove("ed");
-        });
-    }
-
-    const showBody = () => {
-        document.body.style.opacity = 1;
-        setTimeout(() => {
-            addClassToVisibleElements();
-        }, 300);
-    };
-
-    document.addEventListener("scroll", addClassToVisibleElements);
-    window.addEventListener("pageshow", showBody);
 }
+
+document.getElementById("toggleLang").addEventListener("click", e => {
+    console.log("click");
+    const newLang =e.target.getAttribute("data-lang");
+    setCookie("lang", newLang, 365);
+});
+
+// if any <a> been pressed, set body opacity = 0
+document.querySelectorAll("a").forEach(a => {
+    a.addEventListener("click", e => {
+        e.preventDefault();
+        let a = e.target.closest("a"); // Find the closest <a> element (in case of nested elements)
+        if (!a) return;
+        console.log(a);
+        if (a.getAttribute("target") !== "_blank" && !e.ctrlKey && !e.metaKey) {
+            document.body.style.opacity = 0;
+            setTimeout(() => {
+                window.location.href = a.href;
+            }, 300);
+        } else {
+            window.open(a.href, "_blank");
+        }
+    });
+});
+
+// aos
+
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return rect.bottom < 0 || rect.top > window.innerHeight;
+}
+
+function addClassToVisibleElements() {
+    var aosElements = document.querySelectorAll(".aos, li");
+    aosElements.forEach(function (aosElement) {
+        if (!isElementInViewport(aosElement)) aosElement.classList.add("ed");
+        else aosElement.classList.remove("ed");
+    });
+}
+
+const showBody = () => {
+    document.body.style.opacity = 1;
+    setTimeout(() => {
+        addClassToVisibleElements();
+    }, 300);
+};
+
+document.addEventListener("scroll", addClassToVisibleElements);
+window.addEventListener("pageshow", showBody);
